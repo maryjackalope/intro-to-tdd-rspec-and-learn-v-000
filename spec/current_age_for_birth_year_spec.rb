d3da5160ecb1a07e268b3cf8d8e248344f2c5207
Mary Jack <maryjackinthebox@gmail.com>
2018-03-09 04:03:52 +0000

    automatically backed up by learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../current_age_for_birth_year.rb'
 
 def 
-  current_age_for_birth_year
+  current_age_for_birth_year(birth_year)
 end
 
 describe "current_age_for_birth_year method" do
